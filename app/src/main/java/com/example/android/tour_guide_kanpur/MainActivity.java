82d9a7339f566c9eaeeee62ba3233ef59edb6a96
PRAKHAR TWAIRI <prakhartiwari576@gmail.com>
2018-12-09 22:11:40 +0530

    Second commit

--- app/src/main/java/com/example/android/tour_guide_kanpur/MainActivity.java
+++ app/src/main/java/com/example/android/tour_guide_kanpur/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.android.tour_guide_kanpur;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.android.tour_guide_kanpur.R;
+
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
